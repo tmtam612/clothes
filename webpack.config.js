c28c0fbf4ccb4aa8e1515c0d3a139d61713e5277
tmtam612 <tmtam612@gmail.com>
2022-11-25 23:33:15 +0700

    change jenkin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require('webpack').container;
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { ModuleFederationPlugin } = require('webpack').container;
 
-module.exports = ({ mode } = { mode: "production" }) => {
-    console.log(`mode is: ${mode}`);
+// module.exports = ({ mode } = { mode: "production" }) => {
+//     console.log(`mode is: ${mode}`);
 
-    return {
-        mode,
-        entry: "./src/index.tsx",
-        output: {
-            publicPath: "/",
-            path: path.resolve(__dirname, "build"),
-            filename: "bundled.js"
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: "./public/index.html"
-            }),
-        ]
-    }
-};
+//     return {
+//         mode,
+//         entry: "./src/index.tsx",
+//         output: {
+//             publicPath: "/",
+//             path: path.resolve(__dirname, "build"),
+//             filename: "bundled.js"
+//         },
+//         plugins: [
+//             new HtmlWebpackPlugin({
+//                 template: "./public/index.html"
+//             }),
+//         ]
+//     }
+// };
