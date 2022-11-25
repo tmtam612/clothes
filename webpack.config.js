aff8e8e54aeaae8d7d906349e82ba981f71d7796
tmtam612 <tmtam612@gmail.com>
2022-11-25 08:34:04 +0700

    add jenkin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = ({ mode } = { mode: "production" }) => {
     console.log(`mode is: ${mode}`);
